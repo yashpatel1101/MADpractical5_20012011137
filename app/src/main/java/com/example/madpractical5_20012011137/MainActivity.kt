a9e01bee243fc108193de7a632e962c5b6a923b2
YASH <yashpatelys1101@gmail.com>
2022-09-18 21:43:38 +0530

    done

--- app/src/main/java/com/example/madpractical5_20012011137/MainActivity.kt
+++ app/src/main/java/com/example/madpractical5_20012011137/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madpractical5_20012011137
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun play(){
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.DATA_KEY,MyService.DATA_VALUE).apply {
+            startService(this)
+        }
+    }
+    fun stop(){
+        Intent(applicationContext,MyService::class.java).apply {
+            stopService(this)
+        }
+
     }
 }
