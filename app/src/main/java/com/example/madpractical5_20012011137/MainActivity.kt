ae0395ad1611a18cb8cb6b79619b19a14b80dd13
YASH <yashpatelys1101@gmail.com>
2022-09-25 21:52:22 +0530

    done

--- app/src/main/java/com/example/madpractical5_20012011137/MainActivity.kt
+++ app/src/main/java/com/example/madpractical5_20012011137/MainActivity.kt
@@ -4,24 +4,34 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var playbtn : FloatingActionButton
+    private lateinit var stopbtn : FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playbtn = findViewById(R.id.btn_play)
+        playbtn.setOnClickListener(View.OnClickListener {
+            play()
+        })
+
+        stopbtn = findViewById(R.id.btn_stop)
+        stopbtn.setOnClickListener(View.OnClickListener {
+            stop()
+        })
     }
 
-    fun play(view: View) {
-        Intent(applicationContext, MyService::class.java).apply {
-            stopService(this)
-        }
-        fun stop(view: View) {
-            Intent(applicationContext, MyService::class.java).putExtra(
-                MyService.DATA_KEY,
-                MyService.DATA_VALUE
-            ).apply {
-                startService(this)
-            }
-        }
+    fun play() {
+        Intent(applicationContext, MyService::class.java).putExtra(
+            MyService.DATA_KEY,
+            MyService.DATA_VALUE
+        ).apply { startService(this) }
+    }
+
+    fun stop() {
+        Intent(applicationContext, MyService::class.java).apply { stopService(this) }
     }
 }
